aeef4b1d325f6b202e1880fdbfe021cd98319ea7
Daniel Lau <daniellau@Daniels-MacBook-Pro-2.local>
2018-06-18 15:51:13 +0100

    Added ability to deduct balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,7 @@ describe Oystercard do
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
+
     it 'can top up balance' do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
     end
@@ -16,5 +17,15 @@ describe Oystercard do
       expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
   end
+
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'can deduct an amount from the balance' do
+      subject.top_up(20)
+      expect { subject.deduct(5) }.to change{ subject.balance }.by -5
+    end
+  end
+
 
 end
