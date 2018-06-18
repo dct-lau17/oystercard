2a008fd521ed813c1d3dea8db566c5e2a085bb72
jeff1108 <jeffhongkong2007@gmail.com>
2018-06-18 16:35:01 +0100

    added touch in and out

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,9 +12,8 @@ describe Oystercard do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
     end
     it 'raise error if the maximum amount is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+      subject.top_up(Oystercard::MAXIMUM_BALANCE)
+      expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
     end
   end
 
@@ -27,5 +26,24 @@ describe Oystercard do
     end
   end
 
+  describe '#in_journey?' do
+    it 'is initially not in journey' do
+      expect(subject).not_to be_in_journey
+    end
+  end
 
+  describe '#touch_in' do
+    it 'can touch in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  describe '#touch_out' do
+    it 'can touch out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 end
